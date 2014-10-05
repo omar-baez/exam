863a4db467d68534e171df28b952fa78cc70e0d0
omar-baez <isc.omarbaez@gmail.com>
2014-10-05 21:17:55 +0000

    Fancy

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Exam::Application.routes.draw do
 
   resources :items
 
-  resources :micriposts
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
